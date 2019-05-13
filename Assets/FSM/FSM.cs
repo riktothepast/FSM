d0f379f3f0300bc255c47736878c6c79ea464cbd
riktothepast <riktothepast@gmail.com>
2019-05-12 22:07:01 -0600

    Adds FSM and State classes

---
+++ Assets/FSM/FSM.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FiveOTwoStudios.StateMachine
+{
+    public class FSM<T>
+    {
+        protected State<T> currentState;
+
+        public void SetState(State<T> state)
+        {
+            if (currentState != null)
+            {
+                currentState.OnStateExit();
+            }
+            currentState = state;
+            currentState.OnStateEnter();
+        }
+
+        public void Update(float deltaTime)
+        {
+            if (currentState != null)
+            {
+                CheckStateTransitions();
+                currentState.StateUpdate(deltaTime);
+            }
+        }
+
+        void CheckStateTransitions()
+        {
+            foreach (KeyValuePair<Func<bool>, State<T>> transition in currentState.Transitions)
+            {
+                if (transition.Key())
+                {
+                    SetState(transition.Value);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/FSM/State.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FiveOTwoStudios.StateMachine
+{
+    public abstract class State<T>
+    {
+        protected T entity;
+        public Dictionary<Func<bool>, State<T>> Transitions { get; set; }
+
+        public State(T entity)
+        {
+            this.entity = entity;
+            Transitions = new Dictionary<Func<bool>, State<T>>();
+        }
+
+        public void AddTransition(Func<bool> condition, State<T> newState)
+        {
+            Transitions.Add(condition, newState);
+        }
+
+        public abstract void StateUpdate(float deltaTime);
+
+        public abstract void OnStateEnter();
+
+        public abstract void OnStateExit();
+
+    }
+}
